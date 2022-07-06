c98c4e8a83a469e9aa0bd55d095fe04e616e1e51
Alexandra Diaz <alexandradiaz@Alexandras-MacBook-Pro-2.local>
2022-07-05 22:41:11 -0700

    completed destructuring exercise

---
+++ destructuring.js
@@ -0,0 +1,57 @@
+1. 8 , 1846
+
+2. {yearNeptuneDiscovere: 1846, yearMarsDiscovered:1659}
+
+3. Your name is Alejandro and you like purple
+
+your name is Melissa and you like green
+
+your name is undefined and you like green
+
+
+4. Maya
+Marisa 
+Chi 
+
+5. raindrops on roses
+
+whiskers on kittens
+
+[bright copper kettles, warm woolen mittens, brown paper packages tied up with string]
+
+
+6. 10, 30, 20
+
+
+
+// var obj = {
+//     numbers: {
+//       a: 1,
+//       b: 2
+//     }
+//   };
+  
+//   var a = obj.numbers.a;
+//   var b = obj.numbers.b;
+
+const obj ={
+    numbers:{
+        a:1,
+        b:2
+    }
+};
+const {a,b}=obj.numbers;
+
+
+// var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+
+[arr[0], arr[1]]=[arr[1], arr[0]];
+
+
+const raceResults=(first, second, third, fourth, ...rest) => ({first, second, third, fourth, rest })
+
+
+
